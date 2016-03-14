b3209ccdb98a4b9754aa62c459f987cd91a5ed09
irks <irekwrobel23@gmail.com>
2016-03-14 22:29:57 +0100

    Update Liczba.h

--- Liczba.h
+++ Liczba.h
@@ -1,24 +1,19 @@
-//#ifndef LICZBA_H_
-//#define LICZBA_H_
-
 class Liczba 
 { //domyslnie private
 	int calkowita;
 	int ulamkowa;
-	
-	
+
 	public:
 		Liczba(int=0,int=0);
 		~Liczba();
-		void wyswietlLiczbe();
+		void wyswietlLiczbe(); 
+		friend Liczba operator-(Liczba&);
+		Liczba &operator=(Liczba&);
 		friend Liczba operator+(Liczba&, Liczba&);
 		friend Liczba operator-(Liczba&, Liczba&);
 		friend Liczba operator*(Liczba&, Liczba&);
 		friend Liczba operator/(Liczba&, Liczba&);
 		friend bool operator==(Liczba&, Liczba&);
 		friend bool operator>(Liczba&, Liczba&);
-		friend bool operator<(Liczba&, Liczba&);
-		friend Liczba operator-(Liczba&);
-		Liczba &operator=(Liczba&);
+		friend bool operator<(Liczba&, Liczba&);	
 };
-//#endif //LICZBA_H_
