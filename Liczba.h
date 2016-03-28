2f162fd8e4c9ebc773d1f5e2a66ae5a6a5132b56
irks <irekwrobel23@gmail.com>
2016-03-28 19:12:44 +0200

    Update Liczba.h

--- Liczba.h
+++ Liczba.h
@@ -1,19 +1,21 @@
+extern const int PRECYZJA; //stala okreslajaca liczbe cyfr po przecinku
+extern const int POT_10_PRECYZJA;
+
 class Liczba 
 { //domyslnie private
 	int calkowita;
 	int ulamkowa;
 
 	public:
-		Liczba(int=0,int=0);
-		~Liczba();
+		Liczba( int=0, int=0 );
 		void wyswietlLiczbe(); 
-		friend Liczba operator-(Liczba&);
-		Liczba &operator=(Liczba&);
-		friend Liczba operator+(Liczba&, Liczba&);
-		friend Liczba operator-(Liczba&, Liczba&);
-		friend Liczba operator*(Liczba&, Liczba&);
-		friend Liczba operator/(Liczba&, Liczba&);
-		friend bool operator==(Liczba&, Liczba&);
-		friend bool operator>(Liczba&, Liczba&);
-		friend bool operator<(Liczba&, Liczba&);	
+		friend Liczba operator-( const Liczba& );
+		Liczba &operator=( const Liczba& );
+		friend Liczba operator+( const Liczba&, const Liczba& );
+		friend Liczba operator-( const Liczba&, const Liczba& );
+		friend Liczba operator*( const Liczba&, const Liczba&);
+		friend Liczba operator/( const Liczba&, const Liczba& );
+		friend bool operator==( const Liczba&, const Liczba& );
+		friend bool operator>( const Liczba&, const Liczba& );
+		friend bool operator<( const Liczba&, const Liczba& );	
 };
