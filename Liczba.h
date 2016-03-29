a7e3dba0ef94b5fb7d1be8fe3cb89b745ff57e15
irks <irekwrobel23@gmail.com>
2016-03-29 18:48:00 +0200

    Update Liczba.h

--- Liczba.h
+++ Liczba.h
@@ -1,5 +1,8 @@
+// Wrobel Ireneusz, Informatyka, grupa 2I5 
+// Plik naglowkowy, klasa Liczba
+
 extern const int PRECYZJA; //stala okreslajaca liczbe cyfr po przecinku
-extern const int POT_10_PRECYZJA;
+extern const int POT_10_PRECYZJA; // 10 podniesione do potegi PRECYZJA
 
 class Liczba 
 { //domyslnie private
